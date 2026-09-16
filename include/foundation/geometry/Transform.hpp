457251f4ecb94e7f155c62e4bb5e2dd6ea893add
ToxicogenicBees <87375660+ToxicogenicBees@users.noreply.github.com>
2026-09-16 17:11:12 -0400

    Split rotation, position, and scaling to add additional functionality

---
+++ include/foundation/geometry/Transform.hpp
@@ -0,0 +1,240 @@
+/*
+    Transform.hpp
+
+    Declaration of a transform.
+*/
+
+#pragma once
+
+#include "foundation/utility/fp_type.hpp"
+#include "foundation/math/Mat4x4.hpp"
+#include "foundation/geometry/AABB.hpp"
+#include "foundation/math/Vector.hpp"
+#include "foundation/math/Ray.hpp"
+#include <optional>
+
+namespace toxico {
+    class Transform {
+    private:
+        // Cached transformations
+        mutable std::optional<Mat4x4> matrix_;
+        mutable std::optional<Mat4x4> inverse_;
+
+        // Transformation values
+        Mat4x4 rotation_ = Mat4x4::identity();
+        Vector3 position_ = Vector3::zero();
+        Vector3 scale_ = Vector3::one();
+
+        /**
+         * @brief Wipes cached transformations.
+         */
+        void wipeCached_() const noexcept;
+
+        /**
+         * @brief Updates cached transformations.
+         */
+        void updateCached_() const noexcept;
+
+    public:
+        enum class RotationOrder {
+            XYZ,
+            XZY,
+            YXZ,
+            YZX,
+            ZXY,
+            ZYX
+        };
+
+        /**
+         * @brief Constructor.
+         */
+        Transform() = default;
+
+        /**
+         * @brief Applies a translation to the transform in world-space.
+         * 
+         * @param translation The desired translation.
+         */
+        void translate(const Vector3& translation) noexcept;
+
+        /**
+         * @brief Applies a translation to the transform in local-space.
+         * 
+         * @param translation The desired translation.
+         */
+        void translateLocal(const Vector3& translation) noexcept;
+
+        /**
+         * @brief Applies a rotation to the transform.
+         * 
+         * @param rot_x The desired rotation, in radians.
+         */
+        void rotateX(fp_type rot_x) noexcept;
+
+        /**
+         * @brief Applies a rotation to the transform.
+         * 
+         * @param rot_y The desired rotation, in radians.
+         */
+        void rotateY(fp_type rot_y) noexcept;
+
+        /**
+         * @brief Applies a rotation to the transform.
+         * 
+         * @param rot_z The desired rotation, in radians.
+         */
+        void rotateZ(fp_type rot_z) noexcept;
+
+        /**
+         * @brief Applies a rotation to the transform.
+         * 
+         * @param rotation The desired rotation angles, in radians.
+         * @param order The desired rotation order (default is XYZ).
+         */
+        void rotate(const Vector3& rotation, RotationOrder order = RotationOrder::XYZ) noexcept;
+
+        /**
+         * @brief Applies a scaling factor to the transform.
+         * 
+         * @param scale_factor The desired scaling factor.
+         */
+        void scale(const Vector3& scale_factor) noexcept;
+
+        /**
+         * @brief Applies a unified scaling factor to the transform.
+         * 
+         * @param scale_factor The desired unified scaling factor.
+         */
+        void scale(fp_type scale_factor) noexcept;
+
+        /**
+         * @brief Rotates the transform to face a position.
+         * 
+         * @param target The position to focus the transform on.
+         * @param up The desired up-vector (default is the y-axis)
+         */
+        void lookAt(const Vector3& target, const Vector3& up = Vector3::yAxis());
+
+        /**
+         * @brief Gets the position of the transform.
+         * 
+         * @return The position of the transform.
+         */
+        const Vector3& position() const noexcept;
+
+        /**
+         * @brief Gets the scale of the transform.
+         * 
+         * @return The scale of the transform.
+         */
+        const Vector3& scale() const noexcept;
+
+        /**
+         * @brief Gets the rotation of the transform.
+         * 
+         * @return The rotation of the transform.
+         */
+        const Mat4x4& rotation() const noexcept;
+
+        /**
+         * @brief Gets the forward vector of this transform.
+         * 
+         * @return The forward vector of this transform.
+         */
+        Vector3 forward() const;
+
+        /**
+         * @brief Gets the right vector of this transform.
+         * 
+         * @return The right vector of this camera.
+         */
+        Vector3 right() const;
+
+        /**
+         * @brief Gets the up vector of this transform.
+         * 
+         * @return The up vector of this transform.
+         */
+        Vector3 up() const;
+
+        /**
+         * @brief Converts a point from local space to world space.
+         * 
+         * @param point The point being converted.
+         * @return The point in local space.
+         */
+        Vector3 toLocalPoint(const Vector3& point) const;
+
+        /**
+         * @brief Converts a vector from local space to world space.
+         * 
+         * @param point The vector being converted.
+         * @return The vector in local space.
+         */
+        Vector3 toLocalVector(const Vector3& vector) const;
+
+        /**
+         * @brief Converts a normal vector from local space to world space.
+         * 
+         * @param point The normal vector being converted.
+         * @return The normal vector in local space.
+         */
+        Vector3 toLocalNormal(const Vector3& normal) const;
+
+        /**
+         * @brief Converts a ray from local space to world space.
+         * 
+         * @param point The ray being converted.
+         * @return The ray in local space.
+         */
+        Ray3 toLocal(const Ray3& ray) const;
+
+        /**
+         * @brief Converts an AABB from local space to world space.
+         * 
+         * @param point The AABB being converted.
+         * @return The AABB in local space.
+         */
+        AABB toLocal(const AABB& ray) const;
+
+        /**
+         * @brief Converts a point from world space to local space.
+         * 
+         * @param point The point being converted.
+         * @return The point in local space.
+         */
+        Vector3 toWorldPoint(const Vector3& point) const;
+
+        /**
+         * @brief Converts a vector from world space to local space.
+         * 
+         * @param point The vector being converted.
+         * @return The vector in local space.
+         */
+        Vector3 toWorldVector(const Vector3& vector) const;
+
+        /**
+         * @brief Converts a normal vector from world space to local space.
+         * 
+         * @param point The normal vector being converted.
+         * @return The normal vector in local space.
+         */
+        Vector3 toWorldNormal(const Vector3& normal) const;
+
+        /**
+         * @brief Converts a ray from world space to local space.
+         * 
+         * @param point The ray being converted.
+         * @return The ray in local space.
+         */
+        Ray3 toWorld(const Ray3& ray) const;
+
+        /**
+         * @brief Converts an AABB from world space to local space.
+         * 
+         * @param point The AABB being converted.
+         * @return The AABB in local space.
+         */
+        AABB toWorld(const AABB& ray) const;
+    };
+}
---
+++ src/foundation/geometry/Transform.cpp
@@ -0,0 +1,295 @@
+/*
+    Transform.cpp
+
+    Implementation of a transform.
+*/
+
+#include "foundation/geometry/Transform.hpp"
+
+namespace {
+    Vector3 applyPoint(const Mat4x4& mat, const Vector3& point) {
+        const Vector4 homog_point{
+            point.x, point.y, point.z, 1.0
+        };
+        const Vector4 transformed{
+            homog_point.dot(mat.row(0)),
+            homog_point.dot(mat.row(1)),
+            homog_point.dot(mat.row(2)),
+            homog_point.dot(mat.row(3))
+        };
+
+        return {
+            transformed.x / transformed.w,
+            transformed.y / transformed.w,
+            transformed.z / transformed.w
+        };
+    }
+
+    Vector3 applyVector(const Mat4x4& mat, const Vector3& vector) {
+        return {
+            vector.x * mat.at(0, 0) + vector.y * mat.at(0, 1) + vector.z * mat.at(0, 2),
+            vector.x * mat.at(1, 0) + vector.y * mat.at(1, 1) + vector.z * mat.at(1, 2),
+            vector.x * mat.at(2, 0) + vector.y * mat.at(2, 1) + vector.z * mat.at(2, 2)
+        };
+    }
+}
+
+namespace toxico {
+    void Transform::wipeCached_() const noexcept {
+        matrix_.reset();
+        inverse_.reset();
+    }
+
+    void Transform::updateCached_() const noexcept {
+        // Ignore update if the matrices were never wiped
+        if (matrix_ && inverse_)
+            return;
+
+        // Form translation matrix
+        auto translation = Mat4x4::identity();
+        translation(0, 3) = position_.x;
+        translation(1, 3) = position_.y;
+        translation(2, 3) = position_.z;
+
+        // Form scaling matrix
+        auto scaling = Mat4x4::identity();
+        scaling(0, 0) = 1.0 / scale_.x;
+        scaling(1, 1) = 1.0 / scale_.y;
+        scaling(2, 2) = 1.0 / scale_.z;
+
+        // Update cached matrices
+        matrix_ = translation * scaling * rotation_;
+        inverse_ = matrix_->inverse();
+    }
+
+    void Transform::translate(const Vector3& translation) noexcept {
+        position_ += translation;
+        wipeCached_();
+    }
+
+    void Transform::translateLocal(const Vector3& translation) noexcept {
+        position_ += applyPoint(rotation_, translation);
+        wipeCached_();
+    }
+
+    void Transform::rotateX(fp_type rot_x) noexcept {
+        auto transform = Mat4x4::identity();
+        auto sin = std::sin(rot_x);
+        auto cos = std::cos(rot_x);
+
+        transform(1, 1) = cos;
+        transform(1, 2) = -sin;
+        transform(2, 1) = sin;
+        transform(2, 2) = cos;
+
+        rotation_ = transform * rotation_;
+        wipeCached_();
+    }
+
+    void Transform::rotateY(fp_type rot_y) noexcept {
+        auto transform = Mat4x4::identity();
+        auto sin = std::sin(rot_y);
+        auto cos = std::cos(rot_y);
+
+        transform(0, 0) = cos;
+        transform(0, 2) = sin;
+        transform(2, 0) = -sin;
+        transform(2, 2) = cos;
+
+        rotation_ = transform * rotation_;
+        wipeCached_();
+    }
+
+    void Transform::rotateZ(fp_type rot_z) noexcept {
+        auto transform = Mat4x4::identity();
+        auto sin = std::sin(rot_z);
+        auto cos = std::cos(rot_z);
+
+        transform(0, 0) = cos;
+        transform(0, 1) = -sin;
+        transform(1, 0) = sin;
+        transform(1, 1) = cos;
+
+        rotation_ = transform * rotation_;
+        wipeCached_();
+    }
+
+    void Transform::rotate(const Vector3& rotation, RotationOrder order) noexcept {
+        switch(order) {
+        case(RotationOrder::XYZ):
+            rotateX(rotation.x);
+            rotateY(rotation.y);
+            rotateZ(rotation.z);
+            break;
+
+        case(RotationOrder::XZY):
+            rotateX(rotation.x);
+            rotateZ(rotation.z);
+            rotateY(rotation.y);
+            break;
+
+        case(RotationOrder::YXZ):
+            rotateY(rotation.y);
+            rotateX(rotation.x);
+            rotateZ(rotation.z);
+            break;
+
+        case(RotationOrder::YZX):
+            rotateY(rotation.y);
+            rotateZ(rotation.z);
+            rotateX(rotation.x);
+            break;
+
+        case(RotationOrder::ZXY):
+            rotateZ(rotation.z);
+            rotateX(rotation.x);
+            rotateY(rotation.y);
+            break;
+
+        case(RotationOrder::ZYX):
+            rotateZ(rotation.z);
+            rotateY(rotation.y);
+            rotateX(rotation.x);
+            break;
+        }
+    }
+
+    void Transform::scale(const Vector3& scale_factor) noexcept {
+        scale_.x *= scale_factor.x;
+        scale_.y *= scale_factor.y;
+        scale_.z *= scale_factor.z;
+        wipeCached_();
+    }
+
+    void Transform::scale(fp_type scale_factor) noexcept {
+        scale_ *= scale_factor;
+        wipeCached_();
+    }
+
+    void Transform::lookAt(const Vector3& target, const Vector3& up) {
+        // Get the current position of this transform
+        const Vector3 position = toWorldPoint(Vector3::zero());
+
+        // Calculate forward, right, and up vectors
+        const Vector3 forward = (target - position).normal();
+        const Vector3 right = forward.cross(up).normal();
+        const Vector3 corrected_up = right.cross(forward);
+
+        // Update the rotation matrix;
+        rotation_ = Mat4x4::identity();
+        rotation_(0, 0) = right.x;
+        rotation_(1, 0) = right.y;
+        rotation_(2, 0) = right.z;
+        rotation_(0, 1) = corrected_up.x;
+        rotation_(1, 1) = corrected_up.y;
+        rotation_(2, 1) = corrected_up.z;
+        rotation_(0, 2) = -forward.x;
+        rotation_(1, 2) = -forward.y;
+        rotation_(2, 2) = -forward.z;
+        wipeCached_();
+    }
+
+    const Vector3& Transform::position() const noexcept {
+        return position_; 
+    }
+
+    const Vector3& Transform::scale() const noexcept {
+        return scale_; 
+    }
+
+    const Mat4x4& Transform::rotation() const noexcept {
+        return rotation_; 
+    }
+
+    Vector3 Transform::forward() const {
+        return applyVector(rotation_, -Vector3::zAxis());
+    }
+
+    Vector3 Transform::right() const {
+        return applyVector(rotation_, Vector3::xAxis());
+    }
+
+    Vector3 Transform::up() const {
+        return applyVector(rotation_, Vector3::yAxis());
+    }
+
+    Vector3 Transform::toLocalPoint(const Vector3& point) const {
+        updateCached_();
+        return applyPoint(inverse_.value(), point);
+    }
+
+    Vector3 Transform::toLocalVector(const Vector3& vector) const {
+        updateCached_();
+        return applyVector(inverse_.value(), vector);
+    }
+
+    Vector3 Transform::toLocalNormal(const Vector3& normal) const {
+        updateCached_();
+        return applyVector(matrix_->transpose(), normal);
+    }
+
+    Ray3 Transform::toLocal(const Ray3& ray) const {
+        return {
+            toLocalPoint(ray.origin),
+            toLocalVector(ray.direction)
+        };
+    }
+
+    AABB Transform::toLocal(const AABB& aabb) const {
+        // Fetch all 8 corners of the bounding box
+        const auto min = aabb.lowerBound();
+        const auto max = aabb.upperBound();
+        std::array<Vector3, 8> corners;
+        for (std::size_t i = 0; i < 8; ++i) {
+            corners[i].x = ((i & 0x1) ? max.x : min.x);
+            corners[i].y = ((i & 0x2) ? max.y : min.y);
+            corners[i].z = ((i & 0x4) ? max.z : min.z);
+        }
+
+        // Create a bound around the resulting transformed corners
+        AABB result;
+        for (const auto& corner : corners)
+            result.expand(toLocalPoint(corner));
+        return result;
+    }
+
+    Vector3 Transform::toWorldPoint(const Vector3& point) const {
+        updateCached_();
+        return applyPoint(matrix_.value(), point);
+    }
+
+    Vector3 Transform::toWorldVector(const Vector3& vector) const {
+        updateCached_();
+        return applyVector(matrix_.value(), vector);
+    }
+
+    Vector3 Transform::toWorldNormal(const Vector3& normal) const {
+        updateCached_();
+        return applyVector(inverse_->transpose(), normal);
+    }
+
+    Ray3 Transform::toWorld(const Ray3& ray) const {
+        return {
+            toWorldPoint(ray.origin),
+            toWorldVector(ray.direction)
+        };
+    }
+
+    AABB Transform::toWorld(const AABB& aabb) const {
+        // Fetch all 8 corners of the bounding box
+        const auto min = aabb.lowerBound();
+        const auto max = aabb.upperBound();
+        std::array<Vector3, 8> corners;
+        for (std::size_t i = 0; i < 8; ++i) {
+            corners[i].x = ((i & 0x1) ? max.x : min.x);
+            corners[i].y = ((i & 0x2) ? max.y : min.y);
+            corners[i].z = ((i & 0x4) ? max.z : min.z);
+        }
+
+        // Create a bound around the resulting transformed corners
+        AABB result;
+        for (const auto& corner : corners)
+            result.expand(toWorldPoint(corner));
+        return result;
+    }
+}
