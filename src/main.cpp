72ee999bd9d82b115be747aea28d467c4fe3fcb6
ToxicogenicBees <87375660+ToxicogenicBees@users.noreply.github.com>
2026-09-16 17:15:38 -0400

    Update main example

--- src/main.cpp
+++ src/main.cpp
@@ -4,45 +4,53 @@
 
 #include "foundation/utility/fp_type.hpp"
 #include "scene/camera/OrthographicCamera.hpp"
+#include "foundation/containers/SlotArray.hpp"
 #include "scene/geometry/SphereGeometry.hpp"
-#include "scene/Object.hpp"
+#include "scene/geometry/GeometryHandle.hpp"
+#include "scene/query/intersect.hpp"
+#include "scene/object/Object.hpp"
 #include "io/image/ImageWriter.hpp"
 #include "visuals/Image.hpp"
 
 int main() {
-    // Create a unit sphere at the origin
-    SphereGeometry geometry;
-    Object sphere(geometry);
+    // Create a sphere geometry (unit sphere at origin)
+    SlotArray<IGeometry, GeometryHandle> geometries;
+    auto sphere_handle = geometries.insert(std::make_unique<SphereGeometry>());
 
-    // Create a camera
-    const fp_type camera_view_height = 5;
+    // Instantiate a unit sphere at the origin
+    Object sphere(sphere_handle);
+
+    // Create a camera looking at the sphere
+    const fp_type camera_view_height = 10;
     OrthographicCamera camera(camera_view_height);
-
-    // Translate the camera
-    const auto camera_offset = -5 * OrthographicCamera::local_direction;
-    camera.transform().translate(camera_offset);
-
+    camera.transform.translate(-5 * Vector3::xAxis());
+    camera.transform.lookAt(sphere.transform.position());
+    
     // Create an image to be rendered to
-    const Size render_size(1024, 1024);
+    const Size render_size(1280, 720);
     Image image(render_size);
     image.fill(Color4(0, 0, 0, 1));
 
     // Run a raycast to populate the image
-    for (std::size_t x = 0; x < render_size.width; ++x) {
-        for (std::size_t y = 0; y < render_size.width; ++y) {
+    for (std::size_t row = 0; row < render_size.height; ++row) {
+        for (std::size_t col = 0; col < render_size.width; ++col) {
             // Get the pixel being currently viewed through
             const auto pixel = Vector2{
-                static_cast<fp_type>(x),
-                static_cast<fp_type>(y)
+                static_cast<fp_type>(col),
+                static_cast<fp_type>(row)
             };
 
             // Cast a ray through this pixel and check for an intersection
             const auto ray = camera.generateRay(pixel, render_size);
-            const auto result = sphere.intersection(ray);
+            const auto result = intersect(
+                *geometries.resolve(sphere.geometry),
+                sphere.transform,
+                ray
+            );
 
             // Write a pixel if an intersection occurred
             if (result)
-                image.at(x, y) = Color4(1, 1, 1, 1);
+                image.at(row, col) = Color4(1, 1, 1, 1);
         }
     }
 
