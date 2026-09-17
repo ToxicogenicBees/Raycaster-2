78262f02dc6765623281b5f9b1142b7814102325
ToxicogenicBees <87375660+ToxicogenicBees@users.noreply.github.com>
2026-09-16 23:00:32 -0400

    Update main example

--- src/main.cpp
+++ src/main.cpp
@@ -7,12 +7,15 @@
 #include "foundation/utility/fp_type.hpp"
 #include "scene/geometry/utility/GeometryPrimitives.hpp"
 #include "foundation/geometry/Size.hpp"
+#include "rendering/camera/PerspectiveCamera.hpp"
 #include "rendering/camera/OrthographicCamera.hpp"
 #include "rendering/render.hpp"
 #include "scene/Scene.hpp"
 #include "io/image/ImageWriter.hpp"
 #include "visuals/Image.hpp"
 
+#include <iostream>
+#include <format>
 
 int main() {
     // Create a scene
@@ -22,18 +25,19 @@ int main() {
     auto& plane = scene.createObject(GeometryPrimitives::Plane);
     plane.color = Color3(0, 1, 1);
 
-    // Add sphere to scene
+    // Add a sphere to the scene
     auto& sphere = scene.createObject(GeometryPrimitives::Sphere);
     sphere.transform.translate(Vector3::yAxis());
     sphere.color = Color3(1, 0, 0);
 
-    // Create a camera looking at the sphere
-    OrthographicCamera camera(/*view_height*/ 5);
-    camera.transform.translate(-5 * Vector3::xAxis());
+    // Create a perspective camera pointing at the sphere.
+    fp_type fov_90_deg = 90.0 * 3.14159 / 180.0;
+    PerspectiveCamera camera(fov_90_deg);
+    camera.transform.translate({-2, 0.25, 0});
     camera.transform.lookAt(sphere.transform.position());
 
     // Render the image
-    auto image = rendering::render(Size{1280, 720}, scene, camera);
+    auto image = rendering::render(Size{1720, 1080}, scene, camera);
 
     // Write the image to a file
     ImageWriter writer;
