b96cfa76332e773fb03019038157fb8170c811bf
ToxicogenicBees <87375660+ToxicogenicBees@users.noreply.github.com>
2026-09-16 21:08:30 -0400

    Update main example

--- src/main.cpp
+++ src/main.cpp
@@ -1,58 +1,39 @@
 /*
     main.cpp
+
+    An example of rendering a simple scene.
 */
 
 #include "foundation/utility/fp_type.hpp"
-#include "scene/camera/OrthographicCamera.hpp"
-#include "foundation/containers/SlotArray.hpp"
-#include "scene/geometry/SphereGeometry.hpp"
-#include "scene/geometry/GeometryHandle.hpp"
-#include "scene/query/intersect.hpp"
-#include "scene/object/Object.hpp"
+#include "scene/geometry/utility/GeometryPrimitives.hpp"
+#include "foundation/geometry/Size.hpp"
+#include "rendering/camera/OrthographicCamera.hpp"
+#include "rendering/render.hpp"
+#include "scene/Scene.hpp"
 #include "io/image/ImageWriter.hpp"
 #include "visuals/Image.hpp"
 
-int main() {
-    // Create a sphere geometry (unit sphere at origin)
-    SlotArray<IGeometry, GeometryHandle> geometries;
-    auto sphere_handle = geometries.insert(std::make_unique<SphereGeometry>());
 
-    // Instantiate a unit sphere at the origin
-    Object sphere(sphere_handle);
+int main() {
+    // Create a scene
+    Scene scene;
+
+    // Add plane to the scene
+    auto& plane = scene.createObject(GeometryPrimitives::Plane);
+    plane.color = Color3(0, 1, 1);
+
+    // Add sphere to scene
+    auto& sphere = scene.createObject(GeometryPrimitives::Sphere);
+    sphere.transform.translate(Vector3::yAxis());
+    sphere.color = Color3(1, 0, 0);
 
     // Create a camera looking at the sphere
-    const fp_type camera_view_height = 10;
-    OrthographicCamera camera(camera_view_height);
+    OrthographicCamera camera(/*view_height*/ 5);
     camera.transform.translate(-5 * Vector3::xAxis());
     camera.transform.lookAt(sphere.transform.position());
-    
-    // Create an image to be rendered to
-    const Size render_size(1280, 720);
-    Image image(render_size);
-    image.fill(Color4(0, 0, 0, 1));
 
-    // Run a raycast to populate the image
-    for (std::size_t row = 0; row < render_size.height; ++row) {
-        for (std::size_t col = 0; col < render_size.width; ++col) {
-            // Get the pixel being currently viewed through
-            const auto pixel = Vector2{
-                static_cast<fp_type>(col),
-                static_cast<fp_type>(row)
-            };
-
-            // Cast a ray through this pixel and check for an intersection
-            const auto ray = camera.generateRay(pixel, render_size);
-            const auto result = intersect(
-                *geometries.resolve(sphere.geometry),
-                sphere.transform,
-                ray
-            );
-
-            // Write a pixel if an intersection occurred
-            if (result)
-                image.at(row, col) = Color4(1, 1, 1, 1);
-        }
-    }
+    // Render the image
+    auto image = rendering::render(Size{1280, 720}, scene, camera);
 
     // Write the image to a file
     ImageWriter writer;
