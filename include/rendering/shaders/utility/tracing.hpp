ae7cc4257f7c5f87d3b51e3b766c11cb26a24505
ToxicogenicBees <87375660+ToxicogenicBees@users.noreply.github.com>
2026-09-19 17:40:42 -0400

    Move common tracing operations to separate library file

---
+++ include/rendering/shaders/utility/tracing.hpp
@@ -0,0 +1,33 @@
+/*
+    tracing.hpp
+
+    Declaration of a set of free functions to help in tracing.
+*/
+
+#pragma once
+
+#include "scene/light/utility/LightSample.hpp"
+#include "scene/query/SurfaceInteraction.hpp"
+#include "foundation/math/Vector.hpp"
+#include "scene/Scene.hpp"
+
+namespace toxico::tracing {
+    /**
+     * @brief Gets if a light casts a shadow on an object.
+     * 
+     * @param scene The scene containing the light.
+     * @param light_sample The sampled light being checked.
+     * @param point The point in space the shadow would be casted on.
+     * @return If the light casts a shadow on the point.
+     */
+    bool castsShadow(const Scene& scene, const LightSample& light_sample, const SurfaceInteraction& interaction);
+
+    /**
+     * @brief Reflects a vector by a normal vector.
+     * 
+     * @param vector The vector being reflected.
+     * @param normal The normal vector being reflected about.
+     * @return The reflected vector.
+     */
+    Vector3 reflect(const Vector3 vector, const Vector3 normal);
+}
---
+++ src/rendering/shaders/utility/tracing.cpp
@@ -0,0 +1,25 @@
+/*
+    tracing.cpp
+
+    Implementation of a set of free functions to help in tracing.
+*/
+
+#include "rendering/shaders/utility/tracing.hpp"
+#include "foundation/math/Ray.hpp"
+
+namespace toxico::tracing {
+    bool castsShadow(const Scene& scene, const LightSample& light_sample, const SurfaceInteraction& interaction) {
+        const fp_type epsilon = 1e-5;
+        const auto to_light = Ray3{
+            interaction.point + epsilon * interaction.shading_normal,
+            light_sample.direction
+        };
+
+        const auto to_light_interaction = scene.intersect(to_light, epsilon, light_sample.distance - epsilon);
+        return to_light_interaction.has_value();
+    }
+
+    Vector3 reflect(const Vector3 vector, const Vector3 normal) {
+        return 2 * vector.dot(normal) * normal - vector;
+    }
+}
