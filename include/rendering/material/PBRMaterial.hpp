747a5e19c7753e2943ae080a1ba98a7c3879734a
ToxicogenicBees <87375660+ToxicogenicBees@users.noreply.github.com>
2026-09-19 17:42:18 -0400

    Add PBR material

---
+++ include/rendering/material/PBRMaterial.hpp
@@ -0,0 +1,43 @@
+/*
+    PBRMaterial.hpp
+
+    Declaration of a PBR material.
+*/
+
+#pragma once
+
+#include "rendering/material/interfaces/IMaterial.hpp"
+#include "rendering/material/utility/MaterialSample.hpp"
+#include "foundation/utility/fp_type.hpp"
+#include "foundation/math/Vector.hpp"
+#include "rendering/material/utility/Texture.hpp"
+#include "visuals/Image.hpp"
+#include <filesystem>
+
+namespace toxico {
+    class PBRMaterial final : public IMaterial {
+    private:
+        Texture texture_;
+        Texture normal_;
+        MaterialSample sample_;
+
+    public:
+        /**
+         * @brief Constructor.
+         * 
+         * @param texture The desired texture for this material.
+         * @param normal The desired normal map for this material.
+         * @param sample A material sample used to alter the image's material properties.
+         * @param mode The desired texture sampling mode (default is nearest pixel).
+         */
+        PBRMaterial(const std::filesystem::path& texture, const std::filesystem::path& normal, const MaterialSample& sample, Texture::FilterMode mode = Texture::FilterMode::Nearest);
+        
+        /**
+         * @brief Samples the albedo of the material at a UV location.
+         * 
+         * @param uv The UV location on the material.
+         * @return Gets the albedo at the desired UV location.
+         */
+        MaterialSample sample(const Vector2& uv) const noexcept final;
+    };
+}
---
+++ src/rendering/material/PBRMaterial.cpp
@@ -0,0 +1,33 @@
+/*
+    PBRMaterial.cpp
+
+    Implementation of a PBR material.
+*/
+
+#include "rendering/material/PBRMaterial.hpp"
+#include <algorithm>
+
+namespace toxico {
+    PBRMaterial::PBRMaterial(const std::filesystem::path& texture, const std::filesystem::path& normal, const MaterialSample& sample, Texture::FilterMode mode)
+        : texture_(texture, Texture::ColorSpace::sRGB, mode),
+          normal_(normal, Texture::ColorSpace::Linear, mode),
+          sample_(sample)
+    {}
+    
+    MaterialSample PBRMaterial::sample(const Vector2& uv) const noexcept {
+        auto modified_sample = sample_;
+
+        // Set base color from texture
+        modified_sample.base_color = texture_.sample(uv);
+
+        // Set tangent normal from normal map
+        const auto tangent_normal_color = normal_.sample(uv);
+        modified_sample.tangent_normal = Vector3(
+            2.0 * tangent_normal_color.r - 1.0,
+            2.0 * tangent_normal_color.g - 1.0,
+            2.0 * tangent_normal_color.b - 1.0
+        ).normal();
+
+        return modified_sample;
+    }
+}
